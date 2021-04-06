b8d08d259dc293a4679cadaf68ff8e5d08ef9269
avalos <christian@avalosPC.com>
2021-04-05 23:48:49 -0600

    test para metodo findAll en LibroResources creado

--- src/test/java/ues/occ/edu/sv/tpi135/bibliotecawebapp/resources/LibrosResourceTest.java
+++ src/test/java/ues/occ/edu/sv/tpi135/bibliotecawebapp/resources/LibrosResourceTest.java
@@ -5,116 +5,83 @@
  */
 package ues.occ.edu.sv.tpi135.bibliotecawebapp.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ws.rs.core.Response;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import javax.ws.rs.core.Response.ResponseBuilder;
+import static org.junit.Assert.fail;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ues.occ.edu.sv.tpi135.bibliotecawebapp.controller.LibroFacade;
 import ues.occ.edu.sv.tpi135.bibliotecawebapp.entity.Libro;
 
 /**
  *
  * @author aragon598
  */
+@ExtendWith(MockitoExtension.class)
 public class LibrosResourceTest {
-    
+
     public LibrosResourceTest() {
     }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
 
-    /**
-     * Test of findAll method, of class LibrosResource.
-     */
     @Test
     public void testFindAll() {
-        System.out.println("findAll");
-        LibrosResource instance = new LibrosResource();
-        Response expResult = null;
-        Response result = instance.findAll();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        LibrosResource librito = new LibrosResource();//instanciamos la clase a testear
+        LibroFacade libroFacadeMock = Mockito.mock(LibroFacade.class);//mockeamos el facade
+        librito.librofacade = libroFacadeMock;
+        try {
+
+            List<Libro> listaLibros = new ArrayList();
+            listaLibros.add(new Libro(1));
+            Mockito.when(libroFacadeMock.findAll()).thenReturn(listaLibros);//simulamos que al hacer el llamado al metodo findAll nos devuelva una lista de libros
+            librito.findAll();
+            listaLibros = null;
+            Mockito.when(libroFacadeMock.findAll()).thenReturn(listaLibros);//emulamos el caso en quue la lista venga vacia o nula
+            librito.findAll();
+
+        } catch (Exception e) {
+        }
+        try {
+           
+            
+            assertThrows(Exception.class, () -> {
+                 Mockito.doThrow(Exception.class).when(libroFacadeMock).findAll();//cubrimos el escenario en el que se produzca una excepcion al momento de llamar al metodo findAll
+            });
+            librito.findAll();
+
+        } catch (Exception e) {
+        }
+
     }
 
-    /**
-     * Test of findById method, of class LibrosResource.
-     */
     @Test
     public void testFindById() {
-        System.out.println("findById");
-        Integer Id = null;
-        LibrosResource instance = new LibrosResource();
-        Response expResult = null;
-        Response result = instance.findById(Id);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+
     }
 
-    /**
-     * Test of crearLibro method, of class LibrosResource.
-     */
     @Test
     public void testCrearLibro() {
-        System.out.println("crearLibro");
-        Libro libro = null;
-        LibrosResource instance = new LibrosResource();
-        Response expResult = null;
-        Response result = instance.crearLibro(libro);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+
     }
 
-    /**
-     * Test of modificarLibro method, of class LibrosResource.
-     */
     @Test
     public void testModificarLibro() {
-        System.out.println("modificarLibro");
-        Libro libro = null;
-        LibrosResource instance = new LibrosResource();
-        Response expResult = null;
-        Response result = instance.modificarLibro(libro);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+
     }
 
-    /**
-     * Test of deleteLibros method, of class LibrosResource.
-     */
     @Test
     public void testDeleteLibros() {
-        System.out.println("deleteLibros");
-        Integer id = null;
-        LibrosResource instance = new LibrosResource();
-        Response expResult = null;
-        Response result = instance.deleteLibros(id);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+
     }
 
-    /**
-     * Test of obtenerUltimoId method, of class LibrosResource.
-     */
     @Test
     public void testObtenerUltimoId() {
-        System.out.println("obtenerUltimoId");
-        LibrosResource instance = new LibrosResource();
-        Integer expResult = null;
-        Integer result = instance.obtenerUltimoId();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+
     }
-    
 }
